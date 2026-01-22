3dfd7f60133e3448a7afa6182426caf11a9006cd
Ranjit Singh <singhranjeet45757@gmail.com>
2026-01-22 20:34:07 +0100

    done

--- marimogroup1/Ranjit.py
+++ marimogroup1/Ranjit.py
@@ -8,27 +8,142 @@ app = marimo.App(width="medium")
 def _():
     import pandas as pd 
     import marimo as mo
-    return (pd,)
+    import matplotlib.pyplot as plt
+    return mo, pd, plt
 
 
 @app.cell
 def _(pd):
-    url = "https://docs.google.com/spreadsheets/d/1ecopK6oyyb4d_7-QLrCr8YlgFrCetHU7-VQfnYej7JY/export?format=xlsx"
-    dataset = pd.ExcelFile(url, engine='openpyxl')
+    url = "C:/Users/USER/Downloads/airbnb_cleaned.csv"
+    df = pd.read_csv(url)
 
-    sheets = []
-    for sheet in dataset.sheet_names:
-        df = dataset.parse(sheet)
-        df["City"] = sheet #adding a column to track from which group is the data
-        sheets.append(df)
 
-    df = pd.concat(sheets, ignore_index=True)
     return (df,)
 
 
 @app.cell
 def _(df):
-    df
+    cities = sorted(df["City"].unique())
+    room_types = sorted(df["room_type"].unique())
+    host_portfolio = sorted(df["host_portfolio"].unique())
+    return cities, host_portfolio, room_types
+
+
+@app.cell
+def _(cities, mo):
+    selected_cities = mo.ui.multiselect(
+        options=cities,
+        value=cities,
+        label="Select Cities"
+    )
+    return (selected_cities,)
+
+
+@app.cell
+def _(mo, room_types):
+    selected_room_type = mo.ui.dropdown(
+        options=room_types,
+        value=room_types[0],
+        label="Select Room Type"
+    )
+    return (selected_room_type,)
+
+
+@app.cell
+def _(host_portfolio, mo):
+    selected_host_portfolio = mo.ui.dropdown(
+        options= host_portfolio,
+        value=host_portfolio[0],
+        label="Select host Type"
+    )
+    return (selected_host_portfolio,)
+
+
+@app.cell
+def _(df, selected_cities, selected_room_type):
+    filtered_df = df[df["City"].isin(selected_cities.value)]
+
+    counts = (
+        filtered_df
+        .groupby(["City", "room_type"])
+        .size()
+        .unstack(fill_value=0)
+    )
+
+    shares = counts.div(counts.sum(axis=1), axis=0)
+
+    # Select only one room type
+    room_share = shares[selected_room_type.value]
+    return (room_share,)
+
+
+@app.cell
+def _(plt, room_share, selected_room_type):
+    fig, ax = plt.subplots(figsize=(10, 5))
+
+    ax.bar(
+        room_share.index,
+        room_share.values
+    )
+
+    ax.set_title(f"Share of {selected_room_type.value} by City")
+    ax.set_ylabel("Share")
+    ax.set_ylim(0, 1)
+    ax.tick_params(axis="x", rotation=45)
+    return (fig,)
+
+
+@app.cell
+def _(fig, mo, selected_cities, selected_room_type):
+    mo.vstack([
+        mo.md("## Room Type Share by City"),
+        mo.hstack([selected_cities, selected_room_type]),
+        fig
+    ])
+    return
+
+
+@app.cell
+def _(df, selected_cities, selected_host_portfolio):
+    filtered_df1 = df[df["City"].isin(selected_cities.value)]
+
+    counts1 = (
+        filtered_df1
+        .groupby(["City", "host_portfolio"])
+        .size()
+        .unstack(fill_value=0)
+    )
+
+    shares1 = counts1.div(counts1.sum(axis=1), axis=0)
+
+    # Select only one room type
+    host = shares1[selected_host_portfolio.value]
+    return (host,)
+
+
+@app.cell
+def _(host, plt, selected_host_portfolio):
+    figure, ax1 = plt.subplots(figsize=(10, 5))
+
+    ax1.bar(
+        host.index,
+        host.to_numpy()
+    )
+
+    ax1.set_title(f"Share of {selected_host_portfolio.value} by City")
+    ax1.set_ylabel("Share")
+    ax1.set_ylim(0, 1)
+    ax1.tick_params(axis="x", rotation=45)
+    return (figure,)
+
+
+@app.cell
+def _(figure, mo, selected_cities, selected_host_portfolio):
+    mo.vstack([
+        mo.md("## Share of host portfolio by City"),
+        mo.hstack([selected_cities, selected_host_portfolio]),
+        figure
+    ])
     return
 
 
