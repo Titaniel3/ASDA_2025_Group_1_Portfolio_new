f6d4a79c6534bcf8eb4e8c044a2eeb91551d8975
Ranjit Singh <singhranjeet45757@gmail.com>
2026-01-29 15:45:02 +0100

    done

--- marimogroup1/fish_analysis_ranjit.py
+++ marimogroup1/fish_analysis_ranjit.py
@@ -27,12 +27,12 @@ def _():
     import statsmodels.api as sm
 
     from sklearn.model_selection import train_test_split
-    return (pd,)
+    return LinearRegression, pd, train_test_split
 
 
 @app.cell
 def _(pd):
-    df = pd.read_excel("../datasets/Fish_final.xlsx") 
+    df = pd.read_csv("../datasets/Fish.csv") 
     return (df,)
 
 
@@ -43,7 +43,59 @@ def _(df):
 
 
 @app.cell
-def _():
+def _(df):
+    df1 = df.drop(["Length1", "Length3"], axis=1)
+
+    return (df1,)
+
+
+@app.cell
+def _(df1, train_test_split):
+    y = df1[["Species_Bream", "Species_Parkki", "Species_Perch","Species_Pike", "Species_Roach", "Species_Smelt", "Species_Whitefish"]]
+    X = df1[["Length2", "Height", "Width","sqrt_weight"]]   # example clean feature set
+
+    # Split data
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y,
+        test_size=0.2,      # 20% test data
+        random_state=42,    # reproducible
+    )
+
+    print("Train rows:", len(X_train))
+    print("Test rows:", len(X_test))
+    return X_test, X_train, y_train
+
+
+@app.cell
+def _(df):
+    species_cols = [col for col in df.columns if col.startswith("Species_")]
+
+    df["Species"] = df[species_cols].idxmax(axis=1).str.replace("Species_", "")
+
+    return
+
+
+@app.cell
+def _(LinearRegression, X_train, y_train):
+    # Train linear regression model on training data
+
+    # Create the model
+    model = LinearRegression()
+
+    # Train (fit) the model on the training data
+    model.fit(X_train, y_train)
+    return (model,)
+
+
+@app.cell
+def _(X_test, model):
+    y_pred = model.predict(X_test)
+    return (y_pred,)
+
+
+@app.cell
+def _(y_pred):
+    y_pred
     return
 
 
